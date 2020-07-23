50e7cfdf649d816519030ba37e1ccfd0bb172be5
DK <wm2102@naver.com>
2020-07-24 01:08:50 +0900

    Updates

---
+++ precache-manifest.449c795c987535ee5b4ec14005d337ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6451b466b314b312bf691e34c2942c",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "373dd91a27cb3a8c72dd",
+    "url": "/movie_app_2019/static/css/main.6c8904df.chunk.css"
+  },
+  {
+    "revision": "118469322d9ff5a4cfd2",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373dd91a27cb3a8c72dd",
+    "url": "/movie_app_2019/static/js/main.2b5b9a1d.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
