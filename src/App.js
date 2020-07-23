0d1d8299ed25fe9965ddd1ef1e2a75ea742b823f
DK <wm2102@naver.com>
2020-07-24 01:11:38 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Potato({ fav,picture,rating }) {
-  return <div><h1>I like {fav}</h1>
-  <h2>Rating: {rating}/5.0</h2>
-  <img src={picture} alt={fav}></img></div>;
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-const foodILike = [{ id:1,rating:4.5,name: "kimchi", image: "https://static.wixstatic.com/media/d6acfa_09250b1dbbee48d3a2681aef024604ae~mv2_d_1770_1417_s_2.jpg/v1/fill/w_500,h_400,al_c,q_85,usm_0.66_1.00_0.01/d6acfa_09250b1dbbee48d3a2681aef024604ae~mv2_d_1770_1417_s_2.webp" },
-{ id:2,rating:4.1,name: "pork", image: "https://ae01.alicdn.com/kf/HTB1lDOShxrI8KJjy0Fpq6z5hVXan/Wild-2018-Sexy-Micro-Bikini-Set-Beach-Sunbathing-Swim-Costumes-Lingeries-For-Women-Teeny-Swimwear-Female.jpg_640x640.jpg" }];
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
 
-Potato.propTypes={
-  fav: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+  componentDidMount() {
+    this.getMovies();
+  }
 
-function App() {
-  return <div><h1>Hello!</h1>
-  {foodILike.map(dish => <Potato key={dish.id} fav={dish.name} picture={dish.image} rating={dish.rating}/>)}
-  </div>;
+  render() {
+    const { isLoading, movies } = this.state;
+    return <section className="container">
+      {isLoading ?
+        (<div className="loader"><span className="loader_text">Loading..</span></div>)
+        : (<div className="movies">
+          {movies.map(movie => (
+            <Movie key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>)}
+    </section>
+  }
 }
 
 export default App;
